04cd7ba3e1e8bfb29cb99738e1deadb55307a9a1
Geolm <moyaguillaume@gmail.com>
2024-10-09 11:25:21 -0400

    added stateless helpers

---
+++ src/system/stateless.h
@@ -0,0 +1,35 @@
+#ifndef __STATELESS_HELPERS__
+#define __STATELESS_HELPERS__
+
+
+#include <math.h>
+
+ #define SL_TAU    (6.28318530f)
+
+
+static inline float sl_loop(float time_in_second, float loop_in_second)
+{
+    time_in_second /= loop_in_second;
+    return time_in_second - floorf(time_in_second);
+}
+
+static inline float sl_wave(float pos_in_loop)
+{
+    return .5f + .5f * sinf(pos_in_loop * SL_TAU);
+}
+
+// value in [0; 1]
+static inline float sl_impulse(float value)
+{
+    return value * value * value * value;
+}
+
+// pseudo-random
+// returns a value in [0; 1]
+static inline float sl_random(float value)
+{
+    value = cosf(value * 91.3458f) * 47453.5453f;
+    return value - floorf(value);
+}
+
+#endif
