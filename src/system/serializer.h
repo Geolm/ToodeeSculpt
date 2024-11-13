eafc38827bb31b693c6c10f7dc1717019a14b5f5
Geolm <moyaguillaume@gmail.com>
2024-11-13 11:41:22 -0500

    added error status and read functions

--- src/system/serializer.h
+++ src/system/serializer.h
@@ -13,11 +13,19 @@
 //  * avoid overflow
 //-----------------------------------------------------------------------------------------------------------------------------
 
+enum serializer_status
+{
+    serializer_no_error,
+    serializer_write_error,
+    serializer_read_error
+};
+
 typedef struct 
 {
 	uint8_t* buffer;
 	size_t position;	
 	size_t buffer_size;
+    serializer_status status;
 } serializer_context;
 
 //-----------------------------------------------------------------------------------------------------------------------------
@@ -26,6 +34,7 @@ static inline void serializer_init(serializer_context* context, void* buffer, si
     context->buffer = (uint8_t*) buffer;
     context->buffer_size = buffer_size;
     context->position = 0;
+    context->status = serializer_no_error;
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------
@@ -49,18 +58,23 @@ static inline size_t serializer_get_position(serializer_context* context)
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------
-// write functions, return false if there is no enough space to write the data
+static inline enum serializer_status serializer_get_status(serializer_context* context)
+{
+    return context->status;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+// write functions, set the status to error if there is no more space in the buffer
 #define DECLARE_SERIALIZER_WRITE_FUNC(type)                                             \
-static inline bool serializer_write_##type(serializer_context* context, type value)     \
+static inline void serializer_write_##type(serializer_context* context, type value)     \
 {                                                                                       \
     if (context->position + sizeof(type) < context->buffer_size)                        \
     {                                                                                   \
         type* cast = (type*)&context->buffer[context->position];                        \
         *cast = value;                                                                  \
         context->position += sizeof(type);                                              \
-        return true;                                                                    \
     }                                                                                   \
-    return false;                                                                       \
+    else context->status = serializer_write_error;                                      \
 }
 
 DECLARE_SERIALIZER_WRITE_FUNC(float);
@@ -74,29 +88,52 @@ DECLARE_SERIALIZER_WRITE_FUNC(int64_t);
 DECLARE_SERIALIZER_WRITE_FUNC(size_t);
 
 //-----------------------------------------------------------------------------------------------------------------------------
-static inline bool serializer_write_blob(serializer_context* context, void* blob, size_t blob_size)
+static inline void serializer_write_blob(serializer_context* context, const void* blob, size_t blob_size)
 {
     if (serializer_get_leftspace(context) >= blob_size)
     {
         memcpy(&context->buffer[context->position], blob, blob_size);
         context->position += blob_size;
-        return true;
     }
-    return false;
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------
-static inline bool serializer_write_array(serializer_context* context, void* array, size_t count, size_t element_size)
-{
-    if (serializer_get_leftspace(context) >= sizeof(size_t) + count * element_size)
-    {
-        serializer_write_size_t(context, count);
-        serializer_write_blob(context, array, count * element_size);
-        return true;
-    }
-    return false;
+    else context->status = serializer_write_error;
 }
 
 #define serializer_write_struct(context, variable) serializer_write_blob(context, &variable, sizeof(variable))
+
+//-----------------------------------------------------------------------------------------------------------------------------
+// read functions
+#define DECLARE_SERIALIZER_READ_FUNC(type)                                              \
+static inline type serializer_read_##type(serializer_context* context)                   \
+{                                                                                       \
+    if (context->position + sizeof(type) < context->buffer_size)                        \
+    {                                                                                   \
+        type* cast = (type*)&context->buffer[context->position];                        \
+        context->position += sizeof(type);                                              \
+        return *cast;                                                                   \
+    }                                                                                   \
+    context->status = serializer_read_error;                                            \
+    return (type)0;                                                                     \
+}
+
+DECLARE_SERIALIZER_READ_FUNC(float);
+DECLARE_SERIALIZER_READ_FUNC(uint8_t);
+DECLARE_SERIALIZER_READ_FUNC(uint16_t);
+DECLARE_SERIALIZER_READ_FUNC(uint32_t);
+DECLARE_SERIALIZER_READ_FUNC(uint64_t);
+DECLARE_SERIALIZER_READ_FUNC(int8_t);
+DECLARE_SERIALIZER_READ_FUNC(int16_t);
+DECLARE_SERIALIZER_READ_FUNC(int64_t);
+DECLARE_SERIALIZER_READ_FUNC(size_t);
+
+//-----------------------------------------------------------------------------------------------------------------------------
+static inline void serializer_read_blob(serializer_context* context, void* blob, size_t blob_size)
+{
+    if (serializer_get_leftspace(context) >= blob_size)
+    {
+        memcpy(blob, &context->buffer[context->position], blob_size);
+        context->position += blob_size;
+    }
+    else context->status = serializer_read_error;
+}
 
 #endif
