1f4a142af73ae27d363c6f2a93401c48287efa2e
Geolm <moyaguillaume@gmail.com>
2025-02-15 20:55:13 -0500

    decoupling list of primitives and primitive editor

---
+++ src/editor/primitive_list.h
@@ -0,0 +1,30 @@
+#ifndef __PRIMITIVE_LIST_H__
+#define __PRIMITIVE_LIST_H__
+
+#include <stdint.h>
+
+struct primitive;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void plist_init(uint32_t reservation);
+struct primitive* plist_get(uint32_t index);
+void plist_clear(void);
+uint32_t plist_last(void);
+uint32_t plist_size(void);
+void plist_push(struct primitive* p);
+void plist_erase(uint32_t index);
+void plist_insert(uint32_t index, struct primitive* p);
+void plist_resize(uint32_t new_size);
+void plist_terminate(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif
