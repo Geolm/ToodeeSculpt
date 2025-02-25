2638bb2c42bb40f4fcf6a81e37a7f35d154e2225
Geolm <moyaguillaume@gmail.com>
2025-02-25 17:03:51 -0500

    fixed icons radius and updated icons

--- src/icons.c
+++ src/icons.c
@@ -10,7 +10,7 @@ void DrawIcon(struct renderer* gfx_context, aabb box, enum icon_type icon, draw_
     UNUSED_VARIABLE(time_in_second);
     vec2 center = vec2_scale(vec2_add(box.min, box.max), .5f);
     vec2 extent = vec2_scale(vec2_sub(box.max, box.min), .5f);
-    float max_radius = float_min(extent.x, extent.y);
+    float max_radius = vec2_length(extent);
 
     aabb unit_box = (aabb) {.min = vec2_splat(-1.f), vec2_splat(1.f)};
     float loop_1s = sl_loop(time_in_second, 1.f);
@@ -60,7 +60,7 @@ void DrawIcon(struct renderer* gfx_context, aabb box, enum icon_type icon, draw_
         }
     case ICON_DISC:
         {
-            float radius = sl_wave_base(loop_2s, .4f, .3f) * max_radius;
+            float radius = sl_wave_base(loop_2s, .2f, .2f) * max_radius;
             renderer_draw_disc(gfx_context, center, radius, 0.f, fill_solid, primary_color, op_add);
             break;
         }
@@ -69,13 +69,15 @@ void DrawIcon(struct renderer* gfx_context, aabb box, enum icon_type icon, draw_
             vec2 p0 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(.7f, 0.f), vec2_angle(time_in_second)));
             vec2 p1 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.7f, 0.f), vec2_angle(time_in_second)));
             float width = sl_wave_base(loop_2s, .2f, .3f) * max_radius;
-            renderer_draw_orientedbox(gfx_context, p0, p1, width, max_radius * 0.05f, 0.f, fill_solid, primary_color, op_add);
+            renderer_draw_orientedbox(gfx_context, p0, p1, width, 0.f, 0.f, fill_solid, primary_color, op_add);
             break;
         }
     case ICON_ELLIPSE:
         {
-            renderer_draw_ellipse(gfx_context, aabb_bilinear(&box, vec2_set(.3f, .2f)), aabb_bilinear(&box, vec2_set(.7f, .8f)), 
-                                  max_radius * 0.5f, 0.f, fill_solid, primary_color, op_add);
+            vec2 p0 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.5f, -.5f), vec2_angle(-time_in_second)));
+            vec2 p1 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(.5f, .5f), vec2_angle(-time_in_second)));
+            float width = sl_wave_base(loop_2s, .2f, .3f) * max_radius;
+            renderer_draw_ellipse(gfx_context, p0, p1, width, 0.f, fill_solid, primary_color, op_add);
             break;
         }
     case ICON_PIE:
@@ -88,21 +90,24 @@ void DrawIcon(struct renderer* gfx_context, aabb box, enum icon_type icon, draw_
     case ICON_ARC:
         {
             renderer_draw_arc(gfx_context, center, vec2_angle(-time_in_second), sl_wave_base(loop_2s + VEC2_PI, VEC2_PI_2, VEC2_PI_4),
-                             max_radius * 0.5f, max_radius * 0.1f, fill_solid,primary_color, op_add);
+                             max_radius * 0.40f, max_radius * 0.1f, fill_solid,primary_color, op_add);
             break;
         }
     case ICON_TRIANGLE:
         {
-            vec2 p0 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(.4f, .4f), vec2_angle(time_in_second)));
-            vec2 p1 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.1f, -.4f), vec2_angle(time_in_second * -.5f)));
-            vec2 p2 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.2f, .1f), vec2_angle(time_in_second * 1.6f)));
-            renderer_draw_triangle(gfx_context, p0, p1, p2, max_radius * 0.05f, 0.f, fill_solid, primary_color, op_add);
+            vec2 p0 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(.6f, .6f), vec2_angle(time_in_second)));
+            vec2 p1 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.2f, -.5f), vec2_angle(time_in_second * -.5f)));
+            vec2 p2 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.4f, .3f), vec2_angle(time_in_second * 1.6f)));
+            renderer_draw_triangle(gfx_context, p0, p1, p2, 0.f, 0.f, fill_solid, primary_color, op_add);
             break;
         }
     case ICON_CAPSULE:
         {
-            renderer_draw_unevencapsule(gfx_context, aabb_bilinear(&box, vec2_set(.7f, .7f)), aabb_bilinear(&box, vec2_set(.2f, .2f)), 
-                                        max_radius*0.35f, max_radius*0.15f, 0.f, fill_solid, primary_color, op_add);
+            vec2 p0 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(.5f, 0.f), vec2_angle(time_in_second)));
+            vec2 p1 = aabb_project(&unit_box, &box, vec2_rotate(vec2_set(-.5f, 0.f), vec2_angle(time_in_second)));
+            float radius0 = sl_wave_base(loop_2s, .3f, -.2f) * max_radius;
+            float radius1 = sl_wave_base(loop_5s, .3f, -.1f) * max_radius;
+            renderer_draw_unevencapsule(gfx_context, p0, p1, radius0, radius1, 0.f, fill_solid, primary_color, op_add);
             break;
         }
     case ICON_SPLINE:
