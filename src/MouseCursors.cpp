582a6293935e70a5354699bf86c1f15027edb9df
Geolm <moyaguillaume@gmail.com>
2025-10-23 21:38:24 +0200

    fixed high DPI screen : basically do nothing, create the window with the "real size"

--- src/MouseCursors.cpp
+++ src/MouseCursors.cpp
@@ -2,6 +2,7 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <assert.h>
+#include <stdlib.h>
 
 //----------------------------------------------------------------------------------------------------------------------------
 void MouseCursors::Init(struct GLFWwindow* window)
--- src/renderer/MetalLayerHelper.mm
+++ src/renderer/MetalLayerHelper.mm
@@ -36,7 +36,7 @@ void MetalLayerHelper::InitWindow(const char* windowName, unsigned int window_wi
     if (x_scale > 1.f || y_scale > 1.f)
     {
         glfwDestroyWindow(m_Window);
-        m_Window = glfwCreateWindow((window_width*3)/4, (window_height*3)/4, windowName, NULL, NULL);
+        m_Window = glfwCreateWindow(window_width/x_scale, window_height/y_scale, windowName, NULL, NULL);
     }
     
     int width, height;
